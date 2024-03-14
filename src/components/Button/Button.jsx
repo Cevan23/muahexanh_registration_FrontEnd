7af1062ffa5898b5aa65526c9cc5fe4a6b145351
iamt4jdd <chgdat10a3@gmail.com>
2024-03-14 12:49:02 +0700

    Project Detail UI

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,21 @@
+const Button = ({ children, className, type }) => {
+
+
+  const classes = {
+    [className]: className,
+    [type]: type,
+  };
+
+  return (
+    <div className={Object.keys(classes).filter(key => classes[key]).join(' ')}>
+      <button
+        type="button"
+        className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+      >
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
